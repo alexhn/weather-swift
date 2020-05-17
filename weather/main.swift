0b6c25a8b42009af11e8f35a8566b118ca0828b9
Aleksandr Nikiforov <alex@alexn.ee>
2020-05-17 20:29:35 +0300

    Initial Commit

---
+++ weather/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  weather
+//
+//  Created by Aleksandr Nikiforov on 17.05.2020.
+//  Copyright © 2020 Aleksandr Nikiforov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
